a707642069755f012e77f6fe4678332bef18127e
Arthur Silveira <144953268+IsArthurSilveira@users.noreply.github.com>
2024-12-11 23:55:34 +0000

    Segundo Commit

--- PEbit0/ProximaFase.js
+++ PEbit0/ProximaFase.js
@@ -4,24 +4,13 @@ const prompt = require("prompt-sync")();
 function ProximaFase(){
     console.log("Você abre o livro e encontra sua primeira missao resolver um quiz matematico simples.");
     const resposta = prompt("Qual é a resposta para 2 + 2?");
-    if (resposta == 4){
-        console.log("Parabéns! Você acertou a resposta e avançou para a próxima fase.");
-    } else {
-        console.log("Resposta incorreta. Tente novamente.");
-    };
     const resposta2 = prompt("Qual é a resposta para 3 * 3?");
-    if (resposta2 == 9){
-        console.log("Parabéns! Você acertou a resposta e avançou para a próxima fase.");
-    } else {
-        console.log("Resposta incorreta. Tente novamente.");
-    };
     const resposta3 = prompt("Qual é a resposta para 5 - 2?");
-    if (resposta3 == 3){
+    if (resposta == 4 && resposta2 == 9 && resposta3 == 3){
         console.log("Parabéns! Você acertou a resposta e avançou para a próxima fase.");
-    } else {
-        console.log("Resposta incorreta. Tente novamente.");
+    }else{
+        console.log("Respostas incoretas. Tente Novamente");
     };
-    console.log("Parabéns! Você concluiu o quiz e avançou para a próxima fase.");
 };
 
-module.exports = { ProximaFase };
+module.exports = { ProximaFase }
