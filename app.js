46abc2fec7a876159b2feb857b47a389d704ffc7
Arthur Silveira <144953268+IsArthurSilveira@users.noreply.github.com>
2024-12-11 21:52:14 +0000

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const prompt = require ("prompt-sync")();
+
+// Criar Personagem
+
+// Início do jogo
+console.log("Olá! Seja bem-vind@ ao Console.Zero, o coração pulsante de Tecnocife. Sou a IA.Lúcia, mas pode me chamar de Lu. Hoje, serei sua guia nesta jornada.");
+
+// Pergunta Inicial
+const prosseguir = prompt("Deseja prosseguir? (s/n)");
